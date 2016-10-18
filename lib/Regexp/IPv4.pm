b76050de68d50fd39327a1f438653837d93a03c9
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-18 17:12:40 +0700

    Initial commit

---
+++ lib/Regexp/IPv4.pm
@@ -0,0 +1,37 @@
+package Regexp::IPv4;
+
+# DATE
+# VERSION
+
+use strict;
+use warnings;
+
+use Exporter qw(import);
+our @EXPORT_OK = qw($IPv4_re);
+
+my $dig_re = '(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})';
+our $IPv4_re = "(?:$dig_re(?:\\.$dig_re){3})";
+$IPv4_re = qr/$IPv4_re/;
+
+1;
+# ABSTRACT: Regular expression for IPv4 addresses
+
+=head1 SYNOPSIS
+
+ use Regexp::IPvv qw($IPvv_re);
+
+ $address =~ /^$IPv4_re$/ and print "IPv4 address\n";
+
+
+=head1 DESCRIPTION
+
+If you do not use anchor, beware of cases like:
+
+ "255.255.255.256" =~ /($IPv4_re)/; # true & capture "255.255.255.25"
+
+
+=head1 SEE ALSO
+
+L<Regexp::IPv6>
+
+=cut
